dbbf20bef0f560d8b30e0d63108a65dbc315ada1
MrMobbi <julliatmathias@gmail.com>
2023-06-15 12:23:13 +0200

    test

--- srcs/main.c
+++ srcs/main.c
@@ -5,7 +5,7 @@ int	main(int ac, char **av)
 {
 	(void) ac;
 	(void) av;
-	printf("%zu\n", ft_strlen("coucou mathias"));
+	printf("%zu\n", ft_strlen("bisous"));
 	ft_check_arguments();
 	return (0);
 }
