44ed87460c6747650990e0d22a899bef9cc9cf0a
MrMobbi <julliatmathias@gmail.com>
2023-07-11 18:08:50 +0200

    Ray_cast found the wall

--- srcs/ray/print_ray_v2.c
+++ srcs/ray/print_ray_v2.c
@@ -1,6 +1,50 @@
 
 #include "cub3d.h"
 
+t_real	ft_next_wall_e(t_real pos, char **map)
+{
+	t_real	wall;
+
+	while (map[(int)pos.y][(int)pos.x] != '1')
+		pos.x++;
+	wall.y = pos.y;
+	wall.x = pos.x - (pos.x - (int)pos.x);
+	return (wall);
+}
+
+t_real	ft_next_wall_s(t_real pos, char **map)
+{
+	t_real	wall;
+
+	while (map[(int)pos.y][(int)pos.x] != '1')
+		pos.y++;
+	wall.y = pos.y - (pos.y - (int)pos.y);
+	wall.x = pos.x;
+	return (wall);
+}
+
+t_real	ft_next_wall_w(t_real pos, char **map)
+{
+	t_real	wall;
+
+	while (map[(int)pos.y][(int)pos.x] != '1')
+		pos.x--;
+	wall.y = pos.y;
+	wall.x = (int)pos.x + 1;
+	return (wall);
+}
+
+t_real	ft_next_wall_n(t_real pos, char **map)
+{
+	t_real	wall;
+
+	while (map[(int)pos.y][(int)pos.x] != '1')
+		pos.y--;
+	wall.y = (int)pos.y + 1;
+	wall.x = pos.x;
+	return (wall);
+}
+
 t_real	ft_next_wall_se(float facing, t_real pos, char **map)
 {
 	t_real	wall;
@@ -11,35 +55,23 @@ t_real	ft_next_wall_se(float facing, t_real pos, char **map)
 	angle = facing;							// angle is 0 to 90
 	distance.y = (int)pos.y + 1 - pos.y;	// go + in y
 	distance.x = (int)pos.x + 1 - pos.x;	// go + in x
-	
-	printf("distance y[%f]\n", distance.y);
-	printf("distance x[%f]\n", distance.x);
-	printf("angle [%f]\n", angle);
-
 	hypo.y = distance.y / sin(angle);
 	hypo.x = distance.x / cos(angle);
-	printf("hypo y[%f]\n", hypo.y);
-	printf("hypo x[%f]\n", hypo.x);
-
 	if (hypo.y < hypo.x)
 	{
-		printf("Hypo_y SE\n");
 		wall.y = pos.y + distance.y;
 		wall.x = pos.x + (distance.y / tan(angle));
 		if (map[(int)pos.y + 1][(int)pos.x] != '1')
 			wall = ft_next_wall_se(facing, wall, map);
 	}
-	else if (hypo.x < hypo.y)
+	else
 	{
-		printf("Hypo_x SE\n");
 		wall.y = pos.y + (distance.x * tan(angle));
 		wall.x = pos.x + distance.x;
 		if (map[(int)pos.y][(int)pos.x + 1] != '1')
 			wall = ft_next_wall_se(facing, wall, map);
 		
 	}
-	printf("wall y[%f]\n", wall.y);
-	printf("wall x[%f]\n", wall.x);
 	return (wall);
 }
 
@@ -49,42 +81,33 @@ t_real	ft_next_wall_sw(float facing, t_real pos, char **map)
 	t_real	distance;
 	t_real	hypo;
 	float	angle;
+	int		offset;
 
+	offset = 0;
 	angle = M_PI - facing;					// angle is 90 to 180
 	distance.y = (int)pos.y + 1 - pos.y;	// go + in y
 	distance.x = pos.x - (int)pos.x;		// go - in x
 	if (distance.x == 0)
-		distance.x = pos.x - 1;
-	
-	printf("distance y[%f]\n", distance.y);
-	printf("distance x[%f]\n", distance.x);
-	printf("pos y[%f]\n", pos.y);
-	printf("pos x[%f]\n", pos.x);
-	printf("angle [%f]\n", angle);
-
+	{
+		offset = 1;
+		distance.x = 1;
+	}
 	hypo.y = distance.y / sin(angle);
 	hypo.x = distance.x / cos(angle);
-	printf("hypo y[%f]\n", hypo.y);
-	printf("hypo x[%f]\n", hypo.x);
-
 	if (hypo.y < hypo.x)
 	{
-		printf("Hypo_y SW\n");
 		wall.y = pos.y + distance.y;
 		wall.x = pos.x - (distance.y / tan(angle));
-		if (map[(int)pos.y + 1][(int)pos.x] != '1')
+		if (map[(int)pos.y + 1][(int)pos.x - offset] != '1')
 			wall = ft_next_wall_sw(facing, wall, map);
 	}
-	else if (hypo.x < hypo.y)
+	else
 	{
-		printf("Hypo_x SW\n");
 		wall.y = pos.y + (distance.x * tan(angle));
 		wall.x = pos.x - distance.x;
-		if (map[(int)pos.y][(int)pos.x - 1] != '1')
+		if (map[(int)pos.y][(int)pos.x - 1 - offset] != '1')
 			wall = ft_next_wall_sw(facing, wall, map);
 	}
-	printf("wall y[%f]\n", wall.y);
-	printf("wall x[%f]\n", wall.x);
 	return (wall);
 }
 
@@ -94,43 +117,41 @@ t_real	ft_next_wall_nw(float facing, t_real pos, char **map)
 	t_real	distance;
 	t_real	hypo;
 	float	angle;
+	int		offset_x;
+	int		offset_y;
 
+	offset_y = 0;
+	offset_x = 0;
 	angle = facing - M_PI;				// angle is 180 to 270
 	distance.y = pos.y - (int)pos.y;	// go - in y
 	distance.x = pos.x - (int)pos.x;	// go - in x
 	if (distance.y == 0)
-		distance.y = pos.y - 1;
+	{
+		offset_y = 1;
+		distance.y = 1;
+	}
 	if (distance.x == 0)
-		distance.x = pos.x - 1;
-	
-	printf("distance y[%f]\n", distance.y);
-	printf("distance x[%f]\n", distance.x);
-	printf("angle [%f]\n", angle);
-
+	{
+		offset_x = 1;
+		distance.x = 1;
+	}
 	hypo.y = distance.y / sin(angle);
 	hypo.x = distance.x / cos(angle);
-	printf("hypo y[%f]\n", hypo.y);
-	printf("hypo x[%f]\n", hypo.x);
-
 	if (hypo.y < hypo.x)
 	{
-		printf("Hypo_y NW\n");
 		wall.y = pos.y - distance.y;
 		wall.x = pos.x - (distance.y / tan(angle));
-		if (map[(int)pos.y - 1][(int)pos.x] != '1')
+		if (map[(int)pos.y - 1 - offset_y][(int)pos.x - offset_x] != '1')
 			wall = ft_next_wall_nw(facing, wall, map);
 	}
 	else
 	{
-		printf("Hypo_x NW\n");
 		wall.y = pos.y - (distance.x * tan(angle));
 		wall.x = pos.x - distance.x;
-		if (map[(int)pos.y][(int)pos.x - 1] != '1')
+		if (map[(int)pos.y - offset_y][(int)pos.x - 1 - offset_x] != '1')
 			wall = ft_next_wall_nw(facing, wall, map);
 		
 	}
-	printf("wall y[%f]\n", wall.y);
-	printf("wall x[%f]\n", wall.x);
 	return (wall);
 }
 
@@ -140,40 +161,33 @@ t_real	ft_next_wall_ne(float facing, t_real pos, char **map)
 	t_real	distance;
 	t_real	hypo;
 	float	angle;
+	int		offset;
 
+	offset = 0;
 	angle = (2 * M_PI) - facing;			// angle is 270 to 360
 	distance.y = pos.y - (int)pos.y;		// go - in y
 	distance.x = (int)pos.x + 1 - pos.x;	// go + in x
 	if (distance.y == 0)
-		distance.y = pos.y - 1;
-	
-	printf("distance y[%f]\n", distance.y);
-	printf("distance x[%f]\n", distance.x);
-	printf("angle [%f]\n", angle);
-
+	{
+		offset = 1;
+		distance.y = 1;
+	}
 	hypo.y = distance.y / sin(angle);
 	hypo.x = distance.x / cos(angle);
-	printf("hypo y[%f]\n", hypo.y);
-	printf("hypo x[%f]\n", hypo.x);
-
 	if (hypo.y < hypo.x)
 	{
-		printf("Hypo_y NE\n");
 		wall.y = pos.y - distance.y;
 		wall.x = pos.x + (distance.y / tan(angle));
-		if (map[(int)pos.y - 1][(int)pos.x] != '1')
+		if (map[(int)pos.y - 1 - offset][(int)pos.x] != '1')
 			wall = ft_next_wall_ne(facing, wall, map);
 	}
-	else if (hypo.x < hypo.y)
+	else 
 	{
-		printf("Hypo_x NE\n");
 		wall.y = pos.y - (distance.x * tan(angle));
 		wall.x = pos.x + distance.x;
-		if (map[(int)pos.y][(int)pos.x + 1] != '1')
+		if (map[(int)pos.y - offset][(int)pos.x + 1] != '1')
 			wall = ft_next_wall_ne(facing, wall, map);
 	}
-	printf("wall y[%f]\n", wall.y);
-	printf("wall x[%f]\n", wall.x);
 	return (wall);
 }
 /*
@@ -257,22 +271,22 @@ void	ft_print_ray_v2(t_cube *cube, float facing)
 	printf("angle [%d]\n", angle);
 	if (angle == 0)
 	{
-	//	wall = ft_next_wall_e(facing, *cube->player->real, cube->map);
+		wall = ft_next_wall_e(*cube->player->real, cube->map);
 		printf("Looking East\n");
 	}
 	else if (angle == 90)
 	{
-	//	wall = ft_next_wall_s(facing, *cube->player->real, cube->map);
+		wall = ft_next_wall_s(*cube->player->real, cube->map);
 		printf("Looking South\n");
 	}
 	else if (angle == 180)
 	{
-	//	wall = ft_next_wall_w(facing, *cube->player->real, cube->map);
+		wall = ft_next_wall_w(*cube->player->real, cube->map);
 		printf("Looking West\n");
 	}
 	else if (angle == 270)
 	{
-	//	wall = ft_next_wall_n(facing, *cube->player->real, cube->map);
+		wall = ft_next_wall_n(*cube->player->real, cube->map);
 		printf("Looking North\n");
 	}
 	else if (angle > 0 && angle < 90)
