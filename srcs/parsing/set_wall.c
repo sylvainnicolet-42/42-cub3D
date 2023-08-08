db7782d290f1505e9d5489b65f14ab4c50ea4f35
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-08-08 15:46:08 +0200

    Check if texture can be open

--- srcs/parsing/set_wall.c
+++ srcs/parsing/set_wall.c
@@ -1,6 +1,20 @@
 
 #include "cub3d.h"
 
+static bool	ft_can_texture_be_open(char *path)
+{
+	int	fd;
+
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+	{
+		close(fd);
+		return (false);
+	}
+	close(fd);
+	return (true);
+}
+
 void	ft_set_wall(t_cube *cube, char *line)
 {
 	char	**split;
@@ -9,6 +23,8 @@ void	ft_set_wall(t_cube *cube, char *line)
 	if (!split)
 		ft_print_error(MSG_MALLOC_ERR);
 	ft_check_double(cube, split[0]);
+	if (ft_can_texture_be_open(split[1]) == false)
+		ft_print_error(MSG_TEXTURES_CANT_OPEN_ERR);
 	if (ft_strncmp(split[0], "NO", 2) == 0)
 		cube->path_wall_n = ft_strdup(split[1]);
 	else if (ft_strncmp(split[0], "SO", 2) == 0)
