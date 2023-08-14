ab484294741316093707d6568baf1e09ef326f2a
Mathias Julliat <mjulliat@c2r2s1.42lausanne.ch>
2023-08-14 14:34:28 +0200

    norm

--- srcs/actions/move_vision.c
+++ srcs/actions/move_vision.c
@@ -6,7 +6,7 @@
 /*   By: mjulliat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 14:48:48 by mjulliat          #+#    #+#             */
-/*   Updated: 2023/08/14 13:00:51 by mjulliat         ###   ########.fr       */
+/*   Updated: 2023/08/14 14:31:12 by mjulliat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void	ft_move_vision_left(t_cube *cube, int look)
 {
-//	printf("Look Left\n");
+	printf("Look Left\n");
 	cube->player->facing -= ((M_PI * 2) / 360) * look;
 	cube->player->facing = floor(1000000 * cube->player->facing) / 1000000;
 	if (cube->player->facing < 0)
 		cube->player->facing += (M_PI * 2);
-//	printf("Player facing [%f]\n", cube->player->facing);
+	printf("Player facing [%f]\n", cube->player->facing);
 }
 
 void	ft_move_vision_right(t_cube *cube, int look)
 {
-//	printf("Look Right\n");
+	printf("Look Right\n");
 	cube->player->facing += ((M_PI * 2) / 360) * look;
 	cube->player->facing = floor(1000000 * cube->player->facing) / 1000000;
 	if (cube->player->facing > M_PI * 2)
 		cube->player->facing -= (M_PI * 2);
-//	printf("Player facing [%f]\n", cube->player->facing);
+	printf("Player facing [%f]\n", cube->player->facing);
 }
--- srcs/ray/mathias.c
+++ srcs/ray/mathias.c
@@ -6,7 +6,7 @@
 /*   By: synicole <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 10:42:05 by synicole          #+#    #+#             */
-/*   Updated: 2023/08/14 14:10:44 by mjulliat         ###   ########.fr       */
+/*   Updated: 2023/08/14 14:32:02 by mjulliat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_wall	ft_mathias(char **map, t_real *pos, t_wall_values *w, const float *rad)
 {
 	if (pos->y < w->cube->map_max_y)
 	{
-		if(pos->x < w->cube->map_max_x)
+		if (pos->x < w->cube->map_max_x)
 		{	
 			if (map[(int)pos->y][(int)pos->x] == 'D')
 				w->wall.door = true;
@@ -24,15 +24,9 @@ t_wall	ft_mathias(char **map, t_real *pos, t_wall_values *w, const float *rad)
 				w->wall = w->fct(*rad, w->wall.pos, map, w->cube);
 		}
 		else
-		{
 			w->wall.pos.x = w->cube->map_max_x - 1;
-			printf("x[%f]\n", pos->x);
-		}
 	}
 	else
-	{
 		w->wall.pos.y = w->cube->map_max_y - 1;
-		printf("y[%f]\n", pos->y);
-	}
 	return (w->wall);
 }
